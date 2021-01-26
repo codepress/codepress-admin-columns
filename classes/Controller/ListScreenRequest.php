97a8a389bd2156a86aa30197e0b42f5827b2288f
Stefan van den Dungen Gronovius <stefan@codepress.nl>
2021-01-26 10:17:57 +0100

    Preserve List ID per ListScreen for Admin

--- classes/Controller/ListScreenRequest.php
+++ classes/Controller/ListScreenRequest.php
@@ -58,7 +58,6 @@ class ListScreenRequest {
 	 * @return ListScreen
 	 */
 	public function get_list_screen() {
-
 		// Requested list ID
 		$list_id = ListScreenId::is_valid_id( filter_input( INPUT_GET, 'layout_id' ) )
 			? new ListScreenId( filter_input( INPUT_GET, 'layout_id' ) )
@@ -68,7 +67,7 @@ class ListScreenRequest {
 			$list_screen = $this->storage->find( $list_id );
 
 			if ( $list_screen && $this->exists_list_screen( $list_screen->get_key() ) ) {
-				$this->preference->set( 'list_id', $list_screen->get_layout_id() );
+				$this->set_list_id_preference_for_list_screen( $list_screen );
 				$this->preference->set( 'list_key', $list_screen->get_key() );
 
 				return $list_screen;
@@ -81,10 +80,15 @@ class ListScreenRequest {
 		if ( $list_key && $this->exists_list_screen( $list_key ) ) {
 			$this->preference->set( 'list_key', $list_key );
 
-			$list_screen = $this->get_first_available_list_screen( $list_key );
+			// Last visited ID for list sreen
+			$list_screen = $this->get_list_screen_from_list_id_preference( 'list_id_' . $list_key );
+
+			if ( null === $list_screen ) {
+				$list_screen = $this->get_first_available_list_screen( $list_key );
+			}
 
 			if ( $list_screen ) {
-				$this->preference->set( 'list_id', $list_screen->get_layout_id() );
+				$this->set_list_id_preference_for_list_screen( $list_screen );
 
 				return $list_screen;
 			}
@@ -94,17 +98,9 @@ class ListScreenRequest {
 		}
 
 		// Last visited ID
-		$list_id_pref = $this->preference->get( 'list_id' );
-		$list_id = ListScreenId::is_valid_id( $list_id_pref )
-			? new ListScreenId( $list_id_pref )
-			: null;
-
-		if ( $list_id && $this->storage->exists( $list_id ) ) {
-			$list_screen = $this->storage->find( $list_id );
-
-			if ( $list_screen && $this->exists_list_screen( $list_screen->get_key() ) ) {
-				return $list_screen;
-			}
+		$list_screen = $this->get_list_screen_from_list_id_preference( 'list_id' );
+		if ( null !== $list_screen ) {
+			return $list_screen;
 		}
 
 		// Last visited Key
@@ -117,7 +113,7 @@ class ListScreenRequest {
 			$list_screen = $this->get_first_available_list_screen( $list_key );
 
 			if ( $list_screen ) {
-				$this->preference->set( 'list_id', $list_screen->get_layout_id() );
+				$this->set_list_id_preference_for_list_screen( $list_screen );
 
 				return $list_screen;
 			}
@@ -134,13 +130,40 @@ class ListScreenRequest {
 		$list_screen = $this->get_first_available_list_screen( $list_key );
 
 		if ( $list_screen ) {
-			$this->preference->set( 'list_id', $list_screen->get_layout_id() );
+			$this->set_list_id_preference_for_list_screen( $list_screen );
 
 			return $list_screen;
 		}
 
 		// Initialize new
 		return $this->create_list_screen( $list_key );
+	}
+
+	private function set_list_id_preference_for_list_screen( ListScreen $list_screen ) {
+		$this->preference->set( 'list_id_' . $list_screen->get_key(), $list_screen->get_layout_id() );
+		$this->preference->set( 'list_id', $list_screen->get_layout_id() );
+	}
+
+	/**
+	 * @param $key
+	 *
+	 * @return ListScreen|null
+	 */
+	private function get_list_screen_from_list_id_preference( $key ) {
+		$list_id_pref = $this->preference->get( $key );
+		$list_id = ListScreenId::is_valid_id( $list_id_pref )
+			? new ListScreenId( $list_id_pref )
+			: null;
+
+		if ( $list_id && $this->storage->exists( $list_id ) ) {
+			$list_screen = $this->storage->find( $list_id );
+
+			if ( $list_screen && $this->exists_list_screen( $list_screen->get_key() ) ) {
+				return $list_screen;
+			}
+		}
+
+		return null;
 	}
 
 	private function create_list_screen( $key ) {
